87dbb7d05ded4ea0a1c0c62f984aaf055fe0c373
vishal-carvia <vishal.carvia@gmail.com>
2023-08-24 16:41:29 +0530

    Initial commit

---
+++ lib/expenses.dart
@@ -0,0 +1,61 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/new_expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+        title: 'Cinema',
+        amount: 15.69,
+        date: DateTime.now(),
+        category: Category.leisure)
+  ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      // context here is meta data (called context) about expenses widget
+      context: context,
+      // ctx is context of this shwModalBottomSheet, ctx is name we are using for it to avoid conflicts with variable context.
+      builder: (ctx) => const NewExpense(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Expense Tracker'),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          const Text('The chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          )
+        ],
+      ),
+    );
+  }
+}
