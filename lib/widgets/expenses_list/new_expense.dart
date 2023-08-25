5d750936fe7e982c2d2516b98b0bac4f315186a0
vishal-carvia <vishal.carvia@gmail.com>
2023-08-25 13:20:39 +0530

    add error dialog box on invalid input

--- lib/widgets/expenses_list/new_expense.dart
+++ lib/widgets/expenses_list/new_expense.dart
@@ -23,6 +23,22 @@ class _NewExpenseState extends State<NewExpense> {
         (enteredAmount == null) || (enteredAmount <= 0) ? true : false;
     if (_titleController.text.trim().isEmpty || amountIsInvalid) {
       // show error message
+      showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+                title: const Text('Invalid input'),
+                content: const Text(
+                    'Please make sure a valid title, amount, date and category was entered'),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(ctx);
+                    },
+                    child: const Text('Okay'),
+                  )
+                ],
+              ));
+      return;
     }
   }
 
