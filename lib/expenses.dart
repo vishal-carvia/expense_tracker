64ce4e6a3abd99b30703bd9665696b72770d637e
vishal-carvia <vishal.carvia@gmail.com>
2023-09-01 12:47:50 +0530

    make modal bottom sheet responsive

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -30,6 +30,8 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
+      // useSafeArea avoids ui going to device features like punch hole camera on screen. or time and network showing bars on top.
+      useSafeArea: true,
       isScrollControlled: true,
       // context here is a meta data (called context) about expenses widget
       context: context,
@@ -67,6 +69,7 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     Widget mainContent = const Center(
       child: Text("No expenses found. Start adding some!."),
     );
@@ -87,14 +90,25 @@ class _ExpensesState extends State<Expenses> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Chart(expenses: _registeredExpenses),
-          Expanded(
-            child: mainContent,
-          )
-        ],
-      ),
+      body: width < 600
+          ? Column(
+              children: [
+                Chart(expenses: _registeredExpenses),
+                Expanded(
+                  child: mainContent,
+                )
+              ],
+            )
+          : Row(
+              children: [
+                Expanded(
+                  child: Chart(expenses: _registeredExpenses),
+                ),
+                Expanded(
+                  child: mainContent,
+                )
+              ],
+            ),
     );
   }
 }
